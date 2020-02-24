afebe1735fc685a2728941c8ab69c8b931530828
Yu-hanCheng <sarahcheng1231@gmail.com>
2020-02-24 18:45:59 +0800

    opt find nth prime

--- app/Services/FindPrimeService.php
+++ app/Services/FindPrimeService.php
@@ -2,14 +2,12 @@
 namespace App\Services;
 class FindPrimeService{
         public function is_prime($num){
-                $i=2;
                 $odd=3;
-                while($odd<$num && $i<9999){
+                while($odd<$num && $odd<999){
                         if($num%$odd==0){
                                 return 0;
                         }
-                        $i++;
-                        $odd=$i*2+1;
+                        $odd+=2;
                 }
                 return 1;
         }
@@ -17,14 +15,13 @@ class FindPrimeService{
         public function find_nth($n){
                 $a=[2];
                 $len=1;
-                $number=1;
-                while($len<$n && $number<999){
-                        $num=$number*2+1;
+                $num=3;
+                while($len<$n && $num<999){
                         if($this->is_prime($num)){
                                 array_push($a,$num);
                                 $len++;
                         };
-                        $number++;
+                        $num+=2;
                 }
                 return end($a);
         }
